62826eb43479cb6a3940b7b70a171f976e01172f
Diego R. Brogna <dbrogna@dierbro.com>
2024-10-12 07:24:39 +0200

    Initial commit and basic setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+  ],
+  rules: {},
+};
